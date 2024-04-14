f8201e189968612d683057c85d03aebab502694f
malihacodes <maliha0322@gmail.com>
2024-04-15 04:33:04 +0500

    number guessign created

---
+++ index.ts
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const randomNumber = Math.floor(Math.random() * 5);
+
+const answers = await inquirer.prompt([{
+
+
+    name: "UserGuessedNumber",
+    type: "number",
+    message: "Please Guess a number between 1 to 5",
+}]);
+
+if (answers.UserGuessedNumber === randomNumber ){
+
+    console.log("You have guess it right");
+}else{
+    console.log("sorry you have guessed wrong number");
+}
